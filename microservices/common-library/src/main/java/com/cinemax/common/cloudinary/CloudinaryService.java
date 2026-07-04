4ca48de754cd0532ac0377524ace2a90815775dc
Mee72516 <puenterodrigoj@gmail.com>
2026-07-04 15:02:18 -0500

    bases para auth-service

--- microservices/common-library/src/main/java/com/cinemax/common/cloudinary/CloudinaryService.java
+++ microservices/common-library/src/main/java/com/cinemax/common/cloudinary/CloudinaryService.java
@@ -3,6 +3,7 @@ package com.cinemax.common.cloudinary;
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -10,6 +11,7 @@ import java.io.IOException;
 import java.util.Map;
 
 @Service
+@ConditionalOnProperty(prefix = "cloudinary", name = "cloud-name")
 public class CloudinaryService {
 
     private final Cloudinary cloudinary;
