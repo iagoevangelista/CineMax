a64b6ac12cfe8d5e38682a44340eeea075a881d8
Camila <camila.bada@unmsm.edu.pe>
2026-07-07 15:03:26 -0500

    Correción microservicio confiteria-service y cartelera-service

--- microservices/cartelera-service/src/main/java/com/cinemax/cartelera/CarteleraApplication.java
+++ microservices/cartelera-service/src/main/java/com/cinemax/cartelera/CarteleraApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.cinemax.cartelera", "com.cinemax.common"})
 @EnableDiscoveryClient
 public class CarteleraApplication {
     public static void main(String[] args) {
